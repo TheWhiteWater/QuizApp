6d07df98ee20e93afd51dedc1782dac387bdb1cf
TheWhiteWater <ali.agzamov@gmail.com>
2022-05-29 21:10:36 +1200

    start screen, theme, new activity created

--- app/src/main/java/nz/co/redice/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/nz/co/redice/myquizapp/QuizQuestionsActivity.kt
@@ -2,13 +2,10 @@ package nz.co.redice.myquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_quiz_questions)
     }
 }
